f7a4047ec78ac136cee9b0e83d8fb9515c6bb096
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-26 13:59:12 -0400

    last two tests

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTextFormSubmit }) {
+  let [textState, setTextState] = useState("")
+  let [categoryState, setCategoryState] = useState("Code")
+
+  function handleTextState(event) { 
+    setTextState(event.target.value)
+  }
+  function handleCategoryState(event) {
+    setCategoryState(event.target.value)
+  }
+
+  let categoryDropdown = categories.map((category) => {
+    if (category !== "All")
+     return (<option value={category} key={category}>{category}</option>)
+    }
+  )
+
+  function handleOnTextFormSubmit(e){
+    e.preventDefault()
+    onTextFormSubmit( {
+      text: textState,
+      category: categoryState
+    })
+  }
+
   return (
     <form className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input onChange={handleTextState} type="text" name="text" />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select onChange={handleCategoryState} name="category">
+          {categoryDropdown}
         </select>
       </label>
-      <input type="submit" value="Add task" />
+      <input onClick={handleOnTextFormSubmit} type="submit" value="Add task" />
     </form>
   );
 }
